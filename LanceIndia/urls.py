57d3e5958553b8ffbf1c4153602c6a3494a6f42f
Dkg619 <dkgiri619>
2020-06-10 22:05:46 +0530

    UI improved

--- LanceIndia/urls.py
+++ LanceIndia/urls.py
@@ -5,6 +5,7 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage)
+    path('', views.homepage, name='home'),
+    path('services', views.services, name='services')
 
 ]
--- LanceIndia/views.py
+++ LanceIndia/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home.html')
+
+
+def services(request):
+    return render(request, 'services.html')
